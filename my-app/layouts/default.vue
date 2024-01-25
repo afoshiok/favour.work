a1f22277249a797e6295b7e8f734112156ba8a22
Favour Oshiokhale <89757138+afoshiok@users.noreply.github.com>
2024-01-25 02:44:13 -0600

    Added pages and a bento box

---
+++ my-app/layouts/default.vue
@@ -0,0 +1,67 @@
+<template>
+    <Style>
+        div {
+            font-family: 'Heebo', sans-serif;
+        }
+    </Style>
+    <head>
+      <title>Favour Oshiokhale | Data Engineer</title>
+    </head>
+    <div class="h-screen max-h-screen bg-stone-100 relative">
+        <div class="z-[-1]">
+            <NuxtParticles
+            id="tsparticles"
+            :options="options"
+            ></NuxtParticles>
+        </div>
+        <section class="flex flex-col h-lvh justify-between">
+          <header class="flex flex-row justify-between px-10 mt-4 relative z-[1]">
+            <h1 class="font-[800] text-2xl text-green-500">Currently: Open to work (Data Analyst, Data Engineer, Analytics Engineer)</h1>
+            <nav class="grid grid-cols-3 gap-4">
+              <NuxtLink to="/about" class="text-purple-500 font-[800] text-2xl cursor-pointer">About</NuxtLink>
+              <NuxtLink to="/projects" class="text-purple-500 font-[800] text-2xl cursor-pointer">Projects</NuxtLink>
+              <a class="text-purple-500 font-[800] text-2xl">Tool Box</a>
+            </nav>
+          </header>
+
+          <section class="grow">
+              <slot />
+          </section>
+
+          <footer class="pl-10 pb-4 relative z-[1]">
+            <h1 class="text-red-500 text-7xl font-[900]">Favour Oshiokhale</h1>
+            <p class="text-3xl text-orange-500 font-[800] ">Data Engineer based in Houston, TX</p>
+          </footer>
+        </section>
+
+    </div>
+</template>
+
+<script setup lang="ts">
+
+const options = {
+    fullscreen: {
+        enable: true,
+        zIndex: -1
+    },
+    particles: {
+    color: {
+      value: ["#ef4444", "#f97316", "#22c55e", "#3b82f6", "#6366f1", "#8b5cf6"]
+    },
+    move: {
+      enable: true
+    },
+    number: {
+      value: 90
+    },
+    size: {
+        value: 10.5,
+        random: true
+    },
+    shape: {
+        type: ['circle', 'square', 'star', 'triangle']
+    }
+  }
+}
+
+</script>
---
+++ my-app/pages/about/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="h-full mx-10 flex justify-center items-center">
+        <div class="w-3/5 h-4/5 grid grid-rows-8 grid-cols-4 gap-6 relative z-[1]">
+            <div class="col-span-2 row-span-4 bg-red-500 bg-opacity-20 backdrop-filter backdrop-blur-md shadow-lg rounded-3xl p-8">
+                <h2 class="text-2xl font-black">About Me</h2>
+            </div>
+            <div class="row-start-5 col-span-2 row-span-4 bg-red-500 bg-opacity-20 backdrop-filter backdrop-blur-md shadow-lg rounded-3xl p-8">
+                <h2 class="text-2xl font-black">Experience</h2>
+            </div>
+            <div class="col-start-3 col-span-2 row-span-8 bg-red-500 bg-opacity-20 backdrop-filter backdrop-blur-md shadow-lg rounded-3xl p-8">
+                <h2 class="text-2xl font-black">Relevant Coursework / Certificiations </h2>
+            </div>
+        </div>
+    </div>
+</template>
