7a54ad4748f8f43b662ff7e6d667471d8cff8396
Favour Oshiokhale <89757138+afoshiok@users.noreply.github.com>
2024-01-29 12:13:24 -0600

    Update index.vue

--- my-app/pages/about/index.vue
+++ my-app/pages/about/index.vue
@@ -2,20 +2,35 @@
     <section class="h-full overflow overflow-y px-8 mt-4">
         <div class="flex flex-col h-full items-center justify-center">
             <p class="font-black text-lg mx-auto">I'm a <a class="text-purple-700 cursor-pointer" @click="cycleIndex">{{ current_job }}</a> who loves making cool things with data. When I'm not building pipelines and writing SQL queries, I'm watching lots of movies and discovering new songs.</p>
-            <div class="grid grid-cols-2 gap-4 w-3/4 mt-4">
+            <!-- Non-"About Me" Section -->
+            <div class="grid grid-cols-2 grid-rows-2 gap-4 w-3/4 mt-4">
               <div>
                 <h1 class="font-black">Education 🧑🏾‍🎓</h1>
                 <p class="font-medium">I graduated from the <a class="font-bold text-red-600">University of Houston</a> in May 2023 with a <a class="font-bold text-orange-600">B.S. in Computer Information Systems</a>.</p>
               </div>
-                <div>
-                  <h1 class="font-black">Certifications / Accreditations 📜</h1>
-                  <ul class="list-disc list-inside">
-                    <li>Tableau Desktop Specialist (Apr. 2022)</li>
-                    <li>Databricks Lakehouse Fundamentals (Jul. 2023)</li>
-                    <li>Databricks Data Engineer Associate (Sept. 2023)</li>
-                  </ul>
+              <div>
+                <h1 class="font-black">Certifications / Accreditations 📜</h1>
+                <ul class="list-disc list-inside pl-4">
+                  <li>Tableau Desktop Specialist (Apr. 2022)</li>
+                  <li>Databricks Lakehouse Fundamentals (Jul. 2023)</li>
+                  <li>Databricks Data Engineer Associate (Sept. 2023)</li>
+                </ul>
+              </div>
+              <div>
+                <h1 class="font-black">Experience</h1>
+                <div class="flex justify-between">
+                  <h2 class="font-semibold">Slalom Build</h2>
+                  <p>Jun. 2022 - Aug. 2022</p>
+                </div>
+                <p class="text-sm">Data Engineering Intern</p>
+                <ul class="list-disc list-inside pl-4">
+                  <li>Task #1</li>
+                  <li>Task #2</li>
+                  <li>Task #3</li>
+                </ul>
               </div>
             </div>
+            <!-- End of Section -->
         </div>
     </section>
 </template>
