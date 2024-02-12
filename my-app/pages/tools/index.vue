2e676b2d02b686b743d1414bd4c228c82195933a
Favour Oshiokhale <89757138+afoshiok@users.noreply.github.com>
2024-02-11 22:57:53 -0600

    /Tool page almost done

--- my-app/pages/tools/index.vue
+++ my-app/pages/tools/index.vue
@@ -1,13 +1,13 @@
 <template>
     <section class="flex h-full items-center justify-center">
-        <div class="grid grid-rows-8 grid-cols-4 gap-4 w-3/4 h-2/3 px-8">
+        <div class="grid grid-rows-8 grid-cols-4 gap-4 w-3/4 h-2/3 px-8 container my-auto">
             <!-- Languages -->
             <div
             v-motion
             :initial="{ opacity:0, x:-100 }"
             :enter="{ opacity: 1, x: 0, scale: 1 }"
             :delay="300"
-             class=" h-full w-full row-start-1 row-span-3 col-span-1 bg-orange-200 p-4 rounded-xl">
+             class=" h-full w-full row-start-1 col-start-1 row-span-3 col-span-1 bg-orange-200 p-4 rounded-xl">
                 <h1 class="font-bold text-2xl">Languages</h1>
                 <div class="mt-2 flex flex-row flex-wrap">
                     <div class="tooltip tooltip-bottom" data-tip="Python">
@@ -31,10 +31,10 @@
             v-motion
             :initial="{ opacity:0, x:100 }"
             :enter="{ opacity: 1, x: 0, scale: 1 }"
-            :delay="400"
+            :delay="500"
              class=" h-full w-full row-start-1 col-start-2 col-span-3 row-span-3 bg-red-200 rounded-xl p-4">
                 <h1 class="font-bold text-2xl">Data Warehouses and Databases</h1>
-                <div class="flex items-center justify-center">
+                <div class="flex justify-center">
                     <div class="mt-2 flex flex-row flex-wrap">
                         <div class="tooltip tooltip-bottom" data-tip="Snowflake">
                             <SnowflakeIcon class="w-24 h-24 p-4" />
@@ -57,9 +57,39 @@
                         <div class="tooltip tooltip-bottom" data-tip="Oracle">
                             <Oracle class="w-24 h-24 p-4"/>
                         </div>
-                        
                     </div>
                 </div>
+            </div>
+            <div
+            v-motion
+            :initial="{ opacity:0, x:-100 }"
+            :enter="{ opacity: 1, x: 0, scale: 1 }"
+            :delay="700"
+            class=" h-full w-full row-start-4 row-span-3 col-span-2 rounded-xl p-4 bg-green-200">
+                <h1 class="font-bold text-2xl">Cloud Platforms</h1>
+                <div class="mt-2 flex flex-row justify-center">
+                    <AwsIcon class="w-24 h-24 p-4"/>
+                    <AzureIcon class="w-24 h-24 p-4"/>
+                </div>
+            </div>
+            <div
+            v-motion
+            :initial="{ opacity:0, x:100 }"
+            :enter="{ opacity: 1, x: 0, scale: 1 }"
+            :delay="900"
+            class="h-full w-full row-start-4 col-start-3 col-span-2 row-span-4 bg-blue-200 rounded-xl p-4">
+                <h1 class="font-bold text-2xl">Data Tools</h1>
+                <div class="mt-2 flex flex-row flex-wrap justify-center mx-14">
+                    <Dbt class="w-24 h-24 p-4"/>
+                    <ApacheSpark class="w-24 h-24 p-4"/>
+                    <Airflow class="w-24 h-24 p-4"/>
+                    <Jupyter class="w-24 h-24 p-4"/>
+                    <Polars class="w-24 h-24 p-4"/>
+                    <Pandas class="w-24 h-24 p-4"/>
+                </div>
+            </div>
+            <div class="h-full w-full row-start-7 col-span-2 p-4 bg-yellow-200 rounded-xl">
+                <h1>Tools</h1>
             </div>
         </div>
     </section>
