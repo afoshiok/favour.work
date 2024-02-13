3058a74a9444ff6683b5558d84282d1bb04c8b86
Favour Oshiokhale <89757138+afoshiok@users.noreply.github.com>
2024-02-13 14:52:31 -0600

    Added tooltips to icons

--- my-app/pages/tools/index.vue
+++ my-app/pages/tools/index.vue
@@ -7,23 +7,23 @@
             :initial="{ opacity:0, x:-100 }"
             :enter="{ opacity: 1, x: 0, scale: 1 }"
             :delay="300"
-             class=" h-full w-full row-start-1 col-start-1 row-span-3 col-span-1 bg-orange-200 p-4 rounded-xl">
+             class=" h-full w-full row-start-1 col-start-1 row-span-3 col-span-1 bg-orange-200 p-4 rounded-xl max-sm:overflow-y-scroll max-sm:scrollbar-hide">
                 <h1 class="font-bold text-2xl">Languages</h1>
                 <div class="mt-2 flex flex-row flex-wrap">
                     <div class="tooltip tooltip-bottom" data-tip="Python">
-                        <Python  class="w-20 h-20 p-4"/>
+                        <Python  class="w-20 h-20 p-4  4xl:w-24 4xl:h-24"/>
                     </div>
                     <div class="tooltip tooltip-bottom" data-tip="TypeScript">
-                        <Typescript class="w-20 h-20 p-4"/>
+                        <Typescript class="w-20 h-20 p-4  4xl:w-24 4xl:h-24"/>
                     </div>
                     <div class="tooltip tooltip-bottom" data-tip="HTML">
-                        <Hypertext class="w-20 h-20 p-4" />
+                        <Hypertext class="w-20 h-20 p-4  4xl:w-24 4xl:h-24" />
                     </div>
                     <div class="tooltip tooltip-bottom" data-tip="SQL">
-                        <SqlIcon class="w-20 h-20 p-4" />
+                        <SqlIcon class="w-20 h-20 p-4  4xl:w-24 4xl:h-24" />
                     </div>
                     <div class="tooltip tooltip-bottom" data-tip="JavaScript">
-                        <Javascript class="w-20 h-20 p-4"/>
+                        <Javascript class="w-20 h-20 p-4  4xl:w-24 4xl:h-24"/>
                     </div>
                 </div>
             </div>
@@ -37,25 +37,25 @@
                 <div class="flex justify-center">
                     <div class="mt-2 flex flex-row flex-wrap">
                         <div class="tooltip tooltip-bottom" data-tip="Snowflake">
-                            <SnowflakeIcon class="w-24 h-24 p-4" />
+                            <SnowflakeIcon class="w-24 h-24 p-4 4xl:w-32 4xl:h-32" />
                         </div>
                         <div class="tooltip tooltip-bottom" data-tip="MySql">
-                            <Mysql class="w-24 h-24 p-4" />
+                            <Mysql class="w-24 h-24 p-4 4xl:w-32 4xl:h-32" />
                         </div>
                         <div class="tooltip tooltip-bottom" data-tip="DynamoDB">
-                            <Dynamodb class="w-24 h-24 p-4" />
+                            <Dynamodb class="w-24 h-24 p-4 4xl:w-32 4xl:h-32" />
                         </div>
                         <div class="tooltip tooltip-bottom" data-tip="Postgres">
-                            <Postgres class="w-24 h-24 p-4"/>
+                            <Postgres class="w-24 h-24 p-4 4xl:w-32 4xl:h-32"/>
                         </div>
                         <div class="tooltip tooltip-bottom" data-tip="Microsoft SQL Server">
-                            <SqlServer class="w-24 h-24 p-4"/>
+                            <SqlServer class="w-24 h-24 p-4 4xl:w-32 4xl:h-32"/>
                         </div>
                         <div class="tooltip tooltip-bottom" data-tip="Databricks">
-                            <DatabricksIcon class="w-24 h-24 p-4"/>
+                            <DatabricksIcon class="w-24 h-24 p-4 4xl:w-32 4xl:h-32"/>
                         </div>
                         <div class="tooltip tooltip-bottom" data-tip="Oracle">
-                            <Oracle class="w-24 h-24 p-4"/>
+                            <Oracle class="w-24 h-24 p-4 4xl:w-32 4xl:h-32"/>
                         </div>
                     </div>
                 </div>
@@ -68,8 +68,13 @@
             class=" h-full w-full row-start-4 row-span-2 col-span-2 rounded-xl p-4 bg-green-200">
                 <h1 class="font-bold text-2xl">Cloud Platforms</h1>
                 <div class="mt-2 flex flex-row justify-center">
-                    <AwsIcon class="w-24 h-24 p-4"/>
-                    <AzureIcon class="w-24 h-24 p-4"/>
+                    <div class="tooltip tooltip-bottom" data-tip="AWS">
+                        <AwsIcon class="w-24 h-24 p-4 4xl:w-32 4xl:h-32"/>
+                    </div>
+                    <div class="tooltip tooltip-bottom" data-tip="Azure">
+                        <AzureIcon class="w-24 h-24 p-4  4xl:w-32 4xl:h-32"/>
+                    </div>
+                    
                 </div>
             </div>
             <div
@@ -80,22 +85,52 @@
             class="h-full w-full row-start-4 col-start-3 col-span-2 row-span-4 bg-blue-200 rounded-xl p-4">
                 <h1 class="font-bold text-2xl">Data Tools</h1>
                 <div class="mt-2 flex flex-row flex-wrap justify-center mx-14">
-                    <Dbt class="w-24 h-24 p-4"/>
-                    <ApacheSpark class="w-24 h-24 p-4"/>
-                    <Airflow class="w-24 h-24 p-4"/>
-                    <Jupyter class="w-24 h-24 p-4"/>
-                    <Polars class="w-24 h-24 p-4"/>
-                    <Pandas class="w-24 h-24 p-4"/>
-                    <Tableau class="w-24 h-24 p-4"/>
-                    <Powerbi class="w-24 h-24 p-4"/>
+                    <div class="tooltip tooltip-bottom" data-tip="Dbt">
+                        <Dbt class="w-24 h-24 p-4 4xl:w-32 4xl:h-32"/>
+                    </div>
+                    <div class="tooltip tooltip-bottom" data-tip="Apache Spark">
+                        <ApacheSpark class="w-24 h-24 p-4 4xl:w-32 4xl:h-32"/>
+                    </div>
+                    <div class="tooltip tooltip-bottom" data-tip="Apache Airflow">
+                        <Airflow class="w-24 h-24 p-4 4xl:w-32 4xl:h-32"/>
+                    </div>
+                    <div class="tooltip tooltip-bottom" data-tip="Jupyter Notebooks">
+                        <Jupyter class="w-24 h-24 p-4 4xl:w-32 4xl:h-32"/>
+                    </div>
+                    <div class="tooltip tooltip-bottom" data-tip="Polars">
+                        <Polars class="w-24 h-24 p-4 4xl:w-32 4xl:h-32"/>
+                    </div>
+                    <div class="tooltip tooltip-bottom" data-tip="Pandas">
+                        <Pandas class="w-24 h-24 p-4 4xl:w-32 4xl:h-32"/>
+                    </div>
+                    <div class="tooltip tooltip-bottom" data-tip="Tableau">
+                        <Tableau class="w-24 h-24 p-4 4xl:w-32 4xl:h-32"/>
+                    </div>
+                    <div class="tooltip tooltip-bottom" data-tip="Powerbi">
+                        <Powerbi class="w-24 h-24 p-4 4xl:w-32 4xl:h-32"/>
+                    </div>
+                    <div class="tooltip tooltip-bottom" data-tip="Scikit Learn">
+                        <Scikit class="w-24 h-24 p-4 4xl:w-32 4xl:h-32"/>
+                    </div>
                 </div>
             </div>
-            <div class="h-full w-full row-start-6 row-span-2 col-span-2  bg-yellow-200 rounded-xl p-4">
+            <div
+            v-motion
+            :initial="{ opacity:0, x:-100 }"
+            :enter="{ opacity: 1, x: 0, scale: 1 }"
+            :delay="1000" 
+            class="h-full w-full row-start-6 row-span-2 col-span-2  bg-yellow-200 rounded-xl p-4">
                 <h1 class="font-bold text-2xl">Misc. Tools</h1>
                 <div class="mt-2 flex flex-row flex-wrap justify-center">
-                    <Flask class="w-24 h-24 p-4"/>
-                    <Fastapi class="w-24 h-24 p-4"/>
-                    <GithubIcon class="w-24 h-24 p-4"/>
+                    <div class="tooltip tooltip-bottom" data-tip="Flask">
+                        <Flask class="w-24 h-24 p-4 4xl:w-32 4xl:h-32"/>
+                    </div>
+                    <div class="tooltip tooltip-bottom" data-tip="Fast API">
+                        <Fastapi class="w-24 h-24 p-4 4xl:w-32 4xl:h-32"/>
+                    </div>
+                    <div class="tooltip tooltip-bottom" data-tip="Github">
+                        <GithubIcon class="w-24 h-24 p-4 4xl:w-32 4xl:h-32"/>
+                    </div>
                 </div>
             </div>
         </div>
--- my-app/tailwind.config.js
+++ my-app/tailwind.config.js
@@ -2,8 +2,16 @@
 export default {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        '3xl': '1600px',
+        '4xl': '2000px'
+      }
+    },
   },
-  plugins: [require('daisyui')],
+  plugins: [
+    require('daisyui'),
+    require('tailwind-scrollbar-hide')
+  ],
 }
 
