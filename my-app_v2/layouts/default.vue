b689a9fa227e7f207a06384496fc11e3ff615478
afoshiok <89757138+afoshiok@users.noreply.github.com>
2024-04-17 16:24:32 -0500

    minor update

--- my-app_v2/layouts/default.vue
+++ my-app_v2/layouts/default.vue
@@ -1,15 +1,15 @@
 <template>
     <div class="w-dvw h-dvh p-8 bg-zinc-800">
         <section class="w-full mb-2">
-            <nav class="flex flex-row justify-between">
+            <nav class="flex flex-row justify-between align-baseline">
                 <div>
                     <h1 class="text-rose-400 font-semibold text-2xl">FAVOUR OSHIOKHALE</h1>
                     <p class="text-lime-400 font-semibold">Currently: Open to work (Data Engineer, Analytics Engineer, Data Analyst)</p>
                 </div>
                 <div class="flex flex-row gap-4 text-white font-semibold">
-                    <a>Github</a>
-                    <a>Linkedin</a>
-                    <a>Contact</a>
+                    <a class="p-2">Github</a>
+                    <a class="p-2">Linkedin</a>
+                    <a class="bg-rose-400 rounded-md p-2">Contact</a>
                 </div>
             </nav>
         </section>
--- my-app_v2/pages/index.vue
+++ my-app_v2/pages/index.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="h-4/5 flex items-center justify-center">
-        <div class="w-2/3 h-4/5 bg-white">
-
+        <div class="w-2/3 h-2/3">
+            <h1 class="font-bold text-6xl text-white">Hi! I'm Favour and I like making cool things with data. 
+                When I'm not building pipelines 👨‍🔧 and writing SQL statements, I'm watching lots of movies.
+            </h1>
         </div>
     </div>
 </template>
