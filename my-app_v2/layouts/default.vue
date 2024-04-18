88b7f4e369ff3ab0fcfa8a78376a21adc85fb1ba
afoshiok <89757138+afoshiok@users.noreply.github.com>
2024-04-17 20:21:25 -0500

    Adding projects to project page

--- my-app_v2/layouts/default.vue
+++ my-app_v2/layouts/default.vue
@@ -4,12 +4,12 @@
             <nav class="flex flex-row justify-between align-baseline h-12">
                 <div class="p-2 flex flex-col">
                     <h1 class="text-rose-400 font-semibold text-2xl">Favour Oshiokhale</h1>
-                    <p class="text-lime-400 font-semibold">Currently: Open to work (Data Engineer, Analytics Engineer, Data Analyst)</p>
+                    <p class="text-stone-200 font-semibold">Currently: Open to work (Data Engineer, Analytics Engineer, Data Analyst)</p>
                 </div>
                 <div class="flex flex-row gap-4 text-white font-semibold">
                     <NuxtLink to="/projects" class="text-stone-200 p-2 flex items-center">Projects</NuxtLink>
                     <a class="text-stone-200 p-2 flex items-center" href="https://www.linkedin.com/in/favour-oshio/" target="_blank">LinkedIn</a>
-                    <a class="bg-rose-400 text-stone-200 rounded-md px-4 py-2 flex items-center">Contact</a>
+                    <a class="bg-rose-400 text-stone-200 rounded-md px-4 py-2 flex items-center" href="mailto:afoshiokhale@gmail.com">Contact</a>
                 </div>
             </nav>
         </section>
--- my-app_v2/pages/projects/index.vue
+++ my-app_v2/pages/projects/index.vue
@@ -1,17 +1,34 @@
+
+import Project1 from '../../components/project1.vue';
 <template>
     <div class="h-full mt-4">
         <section class="flex justify-center">
             <h1 class="text-stone-200 font-bold text-2xl">Projects</h1>
         </section>
         <section class="mt-4 flex flex-row gap-4">
-            <div class="w-96 h-96 rounded-xl bg-zinc-600">
-
+            <div class="flex flex-col w-96 h-96 rounded-xl bg-zinc-600 p-4">
+                <h1 class="text-stone-200 font-semibold">Letterboard</h1>
+                <div class="w-6/7 h-3/5 rounded-xl bg-zinc-500 mt-2 flex items-center justify-center">
+                    <Letterboard class="w-4/5 h-4/5"/>
+                </div>
+                <div class="mt-2">
+                    <span class="bg-blue-100 text-blue-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">Web Scraping</span>
+                    <span class="bg-red-100 text-red-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded">Streamlit</span>
+                    <span class="bg-orange-100 text-orange-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded">Polars</span>
+                    <span class="bg-orange-100 text-orange-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded">Python</span>
+                </div>
             </div>
-            <div class="w-96 h-96 rounded-xl bg-zinc-600">
+            <div class="w-96 h-96 rounded-xl bg-zinc-600 p-4">
+                <h1 class="text-stone-200 font-semibold">Money + Elections</h1>
+                <div class="w-6/7 h-3/5 rounded-xl bg-zinc-500 mt-2 flex items-center justify-center">
 
+                </div>
             </div>
-            <div class="w-96 h-96 rounded-xl bg-zinc-600">
+            <div class="w-96 h-96 rounded-xl bg-zinc-600 p-4">
+                <h1 class="text-stone-200 font-semibold">Lloyd (WIP)</h1>
+                <div class="w-6/7 h-3/5 rounded-xl bg-zinc-500 mt-2 flex items-center justify-center">
 
+                </div>
             </div>
         </section>
     </div>
