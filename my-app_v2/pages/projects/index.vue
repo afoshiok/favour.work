765d9f0539a65962ab35dd3378439dfe295f7761
Favour Oshiokhale <89757138+afoshiok@users.noreply.github.com>
2024-04-17 22:09:48 -0500

    Project tags

--- my-app_v2/pages/projects/index.vue
+++ my-app_v2/pages/projects/index.vue
@@ -6,25 +6,42 @@ import Project1 from '../../components/project1.vue';
             <h1 class="text-stone-200 font-bold text-2xl">Projects</h1>
         </section>
         <section class="mt-4 flex flex-row gap-4">
-            <div class="flex flex-col w-96 h-96 rounded-xl bg-zinc-600 p-4">
+            <NuxtLink to="/projects/letterboard" class="flex flex-col w-96 h-96 rounded-xl bg-zinc-600 hover:bg-zinc-700 p-4">
                 <h1 class="text-stone-200 font-semibold">Letterboard</h1>
                 <div class="w-6/7 h-3/5 rounded-xl bg-zinc-500 mt-2 flex items-center justify-center">
                     <Letterboard class="w-4/5 h-4/5"/>
                 </div>
                 <div class="mt-2">
-                    <span class="bg-blue-100 text-blue-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">Web Scraping</span>
+                    <span class="bg-blue-100 text-blue-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded">Web Scraping</span>
                     <span class="bg-red-100 text-red-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded">Streamlit</span>
                     <span class="bg-orange-100 text-orange-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded">Polars</span>
-                    <span class="bg-orange-100 text-orange-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded">Python</span>
+                    <span class="bg-yellow-100 text-yellow-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded">Python</span>
                 </div>
-            </div>
-            <div class="w-96 h-96 rounded-xl bg-zinc-600 p-4">
+            </NuxtLink>
+            <div class="w-96 h-96 rounded-xl bg-zinc-600 hover:bg-zinc-700 p-4">
                 <h1 class="text-stone-200 font-semibold">Money + Elections</h1>
                 <div class="w-6/7 h-3/5 rounded-xl bg-zinc-500 mt-2 flex items-center justify-center">
 
                 </div>
+                <div class="mt-2">
+                    <span class="bg-indigo-100 text-indigo-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded">Airflow</span>
+                    <span class="bg-blue-100 text-blue-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded">Snowflake</span>
+                    <span class="bg-yellow-100 text-yellow-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded">Dbt</span>
+                    <span class="bg-red-100 text-red-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded">Pandas</span>
+                    
+                </div>
+                <div class="mt-2">
+                    <span class="bg-lime-100 text-lime-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded">Data Modeling</span>
+                    <span class="bg-orange-100 text-orange-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded">Polars</span>
+                    <span class="bg-blue-100 text-blue-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded">Docker</span>
+                </div>
+                <div class="mt-2">
+                    <span class="bg-lime-100 text-lime-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded">Data Visualization</span>
+                    <span class="bg-lime-100 text-lime-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded">ELT/ETL</span>
+                </div>
+                
             </div>
-            <div class="w-96 h-96 rounded-xl bg-zinc-600 p-4">
+            <div class="w-96 h-96 rounded-xl bg-zinc-600 hover:bg-zinc-700 p-4">
                 <h1 class="text-stone-200 font-semibold">Lloyd (WIP)</h1>
                 <div class="w-6/7 h-3/5 rounded-xl bg-zinc-500 mt-2 flex items-center justify-center">
 
