1a6402179ff00ed94d67aace2c3fbafe59ac41b2
Favour Oshiokhale <89757138+afoshiok@users.noreply.github.com>
2024-04-22 11:55:07 -0500

    website_v2

---
+++ my-app_v2/.nuxt/types/content.d.ts
@@ -0,0 +1,4 @@
+declare module '#content/server' {
+  const serverQueryContent: typeof import('C:/Users/ayoku/Documents/GitHub/favour.cool/my-app_v2/node_modules/@nuxt/content/dist/runtime/legacy/types').serverQueryContent
+  const parseContent: typeof import('C:/Users/ayoku/Documents/GitHub/favour.cool/my-app_v2/node_modules/@nuxt/content/dist/runtime/server').parseContent
+}
